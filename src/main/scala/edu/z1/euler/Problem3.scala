d2c43273dccd1e95892f1a36d8514b1e42cec521
z1 <zafar@linux.com>
2026-09-05 13:21:04 -0400

    problem 3 solution

--- src/main/scala/edu/z1/euler/Problem3.scala
+++ src/main/scala/edu/z1/euler/Problem3.scala
@@ -1,46 +1,44 @@
 
 package edu.z1.euler
 
+import scala.annotation.tailrec
+
 /**
-  *
-  * Largest prime factor
-  *
-  * The prime factors of 13195 are 5, 7, 13 and 29.
-  *
-  * What is the largest prime factor of the number 600851475143 ?
-  * --------------------------------------
-  *
-  * @see https://en.wikipedia.org/wiki/Prime_factor
-  *
-  */
+ *
+ * Largest prime factor
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ * --------------------------------------
+ *
+ * @see https://en.wikipedia.org/wiki/Prime_factor
+ *
+ */
 
 object Problem3 {
 
-  def largestPrimeFactor(below: BigInt): BigInt = {
-   
-   // Tail-recursive helper function to find the largest prime factor
-    @scala.annotation.tailrec
-    def loop(currentNumber: Long, divisor: Long, largest: Long): Long = {
+  def largestPrimeFactor(below: BigInt): List[BigInt] = {
+
+    // Tail-recursive helper to collect factors into a list efficiently
+    @tailrec
+    def loop(currentNumber: BigInt, divisor: BigInt, acc: List[BigInt]): List[BigInt] = {
       if (currentNumber <= 1) {
-        largest
+        acc.reverse // Reverse to maintain ascending order
       } else if (currentNumber % divisor == 0) {
-        // 'divisor' is a factor, divide it out completely and update 'largest'
-        loop(currentNumber / divisor, divisor, divisor)
+        // Found a factor, store it and keep dividing the number by it
+        loop(currentNumber / divisor, divisor, divisor :: acc)
       } else if (divisor * divisor > currentNumber) {
-        // If the square of the divisor exceeds the remaining number, 
-        // the remaining number itself must be prime and the largest factor.
-        currentNumber
+        // If divisor squared exceeds the remaining number,
+        // the remaining number itself must be a prime factor.
+        (currentNumber :: acc).reverse
       } else {
-        // Move to the next potential divisor
-        // Optimization: skip even numbers after 2
-        val nextDivisor = if (divisor == 2) 3 else divisor + 2
-        loop(currentNumber, nextDivisor, largest)
+        // Move to the next potential odd divisor
+        val nextDivisor = if (divisor == 2) BigInt(3) else divisor + 2
+        loop(currentNumber, nextDivisor, acc)
       }
     }
 
-    loop(below, 2, 1)
+    if (below <= 1) Nil else loop(below, 2, Nil)
   }
-}
-
-
 }
--- src/test/scala/Problem3Spec.scala
+++ src/test/scala/Problem3Spec.scala
@@ -5,22 +5,24 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 /**
-  * Largest prime factor
-  *
-  * The prime factors of 13195 are 5, 7, 13 and 29.
-  *
-  * What is the largest prime factor of the number 600851475143 ?
-  */
-class Problem3Spec() extends AnyFlatSpec with should.Matchers {
+ * Largest prime factor
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+class Problem3Spec extends AnyFlatSpec with should.Matchers {
 
-  "P3: prime factors of 5, 7, 13 and 29 " should " be equal 13195" in {
-    pending
-    assert(Problem3.largestPrimeFactor(600851475143L) == 13195)
+  val fixture: Problem3.type = Problem3
+
+  "P3: prime factors of 13195" should "be equal to 5, 7, 13, 29" in {
+    assert(fixture.largestPrimeFactor(13195) == List(5, 7, 13, 29))
+    assert(fixture.largestPrimeFactor(13195).max == 29)
   }
 
-  "P3: prime factor of 600851475143L " should " be equal ???" in {
-    pending
-    assert(Problem3.largestPrimeFactor(600851475143L) == ???)
+  "P3: prime factor of 600851475143L " should " be equal to 5857" in {
+    assert(fixture.largestPrimeFactor(600851475143L) == List(71, 839, 1471, 6857))
+    assert(fixture.largestPrimeFactor(600851475143L).max == 6857)
   }
 
 }
